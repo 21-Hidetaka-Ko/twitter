d1316ad0f92d26313d3d994d6bdf8d3e7491b206
Hidetaka <hidetaka.48691001@icloud.com>
2015-08-21 18:41:37 +0900

    finish

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '9e16c34435c5deb5982eff21285fe59056a99cbbba1b7f20b223deaab1b286e2c321e82a15d34df3e965aab437d04ddf9ba958a63925493a01e5e63e76743a8f'
